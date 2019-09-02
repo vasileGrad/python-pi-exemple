3e25ebef0506bddec9b1c0ac2fabc9e120306039
grad <vasile.grad2@gmail.com>
2019-09-02 19:25:50 +0300

    Newest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-exemple
 This is an example project to demonstrate the use of git on Ubuntu.
+
+Make a change
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def main(msg):
+    // Another comment
     // Adding the second comment to this file.
     // Print a message
     print(msg)
